3ca9dd2966c37e5ce64d42f5d8b7f3606a8fa72e
Delian Petrov <delian@fridgeguideai.com>
2024-10-16 15:27:38 -0700

    rotating the google maps platform api key

--- config/initializers/vcr.rb
+++ config/initializers/vcr.rb
@@ -3,5 +3,6 @@ if Rails.env.test?
     config.cassette_library_dir = "fixtures/vcr_cassettes"
     config.hook_into :webmock
     config.filter_sensitive_data("<API_KEY>") { Rails.application.credentials.dig(Rails.env.to_sym, :open_weather_api_key) }
+    config.filter_sensitive_data("<API_KEY>") { Rails.application.credentials.dig(Rails.env.to_sym, :google_geocoder_api_key) }
   end
 end
